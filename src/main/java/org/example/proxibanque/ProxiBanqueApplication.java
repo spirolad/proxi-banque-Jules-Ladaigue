1b8e7c583a93ecfa3c0af37c875d99f6815a384a
Spirolad <jules.ladaigue@epita.fr>
2025-11-20 10:32:07 +0100

    feat: init

---
+++ src/main/java/org/example/proxibanque/ProxiBanqueApplication.java
@@ -0,0 +1,13 @@
+package org.example.proxibanque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxiBanqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxiBanqueApplication.class, args);
+    }
+
+}
